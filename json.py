676b0fe356df2258960b660f708f956cbbd057a3
marisujith22 <162672337+marisujith22@users.noreply.github.com>
2025-09-16 09:58:08 +0530

    Add files via upload

--- json.py
+++ json.py
@@ -1,128 +1,111 @@
-import re
-def parser_json(json_string : str):
-    parsed_data = _parser_value(json_string.strip())
-    return JSONSearchable(parsed_data)
+import re        
+class JSONSearchable:
+    def __init__(self, data):
+        self.data=data
+    
+    def parse(json_string):
+        try:
+          for old,new in [("true","True"),("false","False"),("null","None")]:
+            json_string=json_string.replace(old,new)
+          json_string=json_string.strip()  
+          return JSONSearchable (eval(json_string,{ "__builtins__":None },{}))
+        except Exception as e:
+            print("indentation error:",e)
+            return None
+       
+    def search(self,path):
+        parts=re.split(r'\.(?![^\[]*\])',path)
+        current_data=self.data
+        for value in parts:
+            if not value:
+                return None
+            if "[" in value:
+                key,*rest=re.split(r'(\[.*?\])',value) 
+                if key:
+                    current_data=current_data.get(key)
+                    
+                for r in rest:
+                    if not r:
+                        continue
+                    if r.startswith("[?"):
+                        obj, op, val=re.split(r'(==|!=|>=|<=|>|<)',r[2:-1]) 
+                        obj,value=obj.strip(),value.strip().strip('"') 
+                        if val.replace('.','',1).isdigit():
+                            val=float(val)
+                        elif val=="true":
+                            val=True
+                        elif val=="false":
+                            val = False
+                        elif val=="null":
+                            val=None
+                                    
+                        res=[]
+                        for item in current_data:
+                            if obj in item:
+                                try:
+                                    if eval(f"item[obj] {op} val"):
+                                            res.append(item)
+                                except Exception as e:
+                                    print("eval error:",e)
+                                   
+                        current_data=res                                                                
+                                            
+                    else:
+                        idx=int(r[1:-1])
+                        current_data=current_data[idx]
+                        if isinstance:
+                            return current_data,dict
+                        else:
+                            return None         
+            else:
+                
+                if isinstance(current_data,dict):
+                    current_data=current_data.get(value)  
+                elif isinstance(current_data,list):
+                    temp=[]
+                    for item in current_data:
+                        if isinstance(item,dict) and value in item:
+                            temp.append(item[value])
+                    current_data=temp
+                            
+                else:
+                    return None
+                if current_data is None:
+                    return None
+                
+                
+        return current_data                      
 
-
-#Regex Patterns
-STRING_RE = r'"((?:\\.|[^"\\])*)"'
-NUMBER_RE = r'-?\d+(?:\.\d+)?(?:[eE][+-]?\d+)?'
-BOOLEAN_RE = r'true|false'
-NULL_RE = r'null'
-
-
-def _parser_value(text):
-    text = text.strip()
-    if text.startswith('{'):
-        return _parser_object(text)
-    elif text.startswith('['):
-        return _parser_array(text)
-    elif text.startswith('"'):
-        return _parser_string(text)
-    elif text.startswith(r'^-?\d', text):
-        return _parser_number(text)
-    elif text.startswith('true') or text.startswith('false'):
-        return _parser_boolean(text)
-    elif text.startswith('null'):
-        return _parser_null(text)
+if __name__=="__main__":
+    json_data = """
+    {
+        "store": "Main Street Books",
+        "inventory": [
+        { "type": "book", "title": "The Great Gatsby", "price": 12.50, "in_stock": true },
+        { "type": "book", "title": "Moby Dick", "price": 15.00, "in_stock": false },
+        { "type": "magazine", "title": "Tech Today", "price": 5.99, "in_stock": true }
+        ],
+        "location": {
+        "city": "New York",
+        "postcode": "10001"
+        }
+    }
+    """   
+    searchable_json = JSONSearchable.parse(json_data)
+    if searchable_json is None:
+        print("parse fail")
     else:
-        raise ValueError("invalid JSON value")
+        print("parse not fail")    
 
-
-
-def _parser_object(text):
-    assert text.startswith('{')
-    obj = {}
-    text = text[1:].strip()
-    while not text.startswith('}'):
-        key, text.strip()
-        if not text.startswith(':'):
-            raise ValueError("Expected ':' after key")
-        text = text[1:].strip()
-        value, text = _parser_value(text)
-        obj[key] = value
-        text = text.strip()
-        if text.startswith(','):
-            text = text[1:].strip()
-        elif text.startswith('}'):
-            break
-        else:
-            raise ValueError("Expected ',' or '}' in object")
-        return obj, text[1:]
-
-
-
-def _parser_array(text):
-    assert text.startswith('[')
-    ar = []
-    text = text[1:].strip()
-    while not text.startswith(']'):
-        value, text = _parser_value(text)
-        arr.append(value)
-        text = text.strip()
-        if text.startswith(','):
-            text = text[1:].strip()
-        elif text.startswith(']'):
-            break
-        else:
-            raise ValueError("Expected ',' or ']' in array")
-        return arr, text[1:] 
-
-
-def _parser_string(text):
-
-    match = re.match(STRING_RE, text)
-    if not match:
-        raise ValueError("Invalid JSON string")
-
-
-    s = match.group(1)
-    s = s.replace('\\"', '"').replace('\\\\', '\\')
-    rest = text[match.end():]
-    return s, rest
-
-
-
-def _parser_number(text):
-    match = re.match(NUMBER_RE, text)
-
-
-    if not match:
-        raise ValueError("Invalid JSON number")
-
-
-    num_str = match.group(0)
-    if '.' in num_str or 'e' in num_str or 'E' in num_str:
-
-        value = float(num_str)
-
-    else:
-        value = int(num_str)
-
-    rest = text[match.end():]
-    return value, rest
-
-
-
-def _parser_boolean(text):
-
-    if text.startswith('true'):
-        return True, text[4:]
-
-    elif text.startswith('false'):
-        return False, text[5:]
-
-    else:
-        raise ValueError("Invalid JSON number")     
-                         
-def _parser_null(text):
-
-    if text.startswith('null'):
-        return Null, text[4:]
-
-    else:
-        raise ValueError("Invalid JSON null")
-
-
-
-def _parser_           
+    for Queries in [
+        "store",
+        "inventory[0].title",
+        "inventory[?in_stock==true]",
+        "inventory[?price<15.0].title",
+        "location.country"
+    ]:
+        my_variable=searchable_json
+        if my_variable is not None:
+            result=my_variable.search(Queries)
+            print(Queries,"--",result)
+        print(searchable_json.search("store"))    
